80db61f987c981761aa61db686ed2ddbd8a72a88
Josh O <josh.owen121@gmail.com>
2022-10-29 13:36:44 +0100

    clean up oled code

--- keyboards/lily58/keymaps/miryoku_plus/keymap.c
+++ keyboards/lily58/keymaps/miryoku_plus/keymap.c
@@ -1,9 +1,6 @@
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
-//char wpm_str[10];
-//uint_t wpm_graph_timer = 0;
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // COLEMAK-DH
 	[0] = LAYOUT(
@@ -72,11 +69,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 //SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
 #ifdef OLED_ENABLE
-  //oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    //if (!is_keyboard_master())
-    //  return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-   // return rotation;
-  //}
+  oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+    //return rotation;
+  }
 
   // When you add source files to SRC in rules.mk, you can use functions.
   const char *read_layer_state(void);
@@ -85,127 +81,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   const char *read_keylog(void);
   const char *read_keylogs(void);
 
-  // const char *read_mode_icon(bool swap);
-  // const char *read_host_led_state(void);
-  // void set_timelog(void);
-  // const char *read_timelog(void);
+  //const char *read_mode_icon(bool swap);
+  //const char *read_host_led_state(void);
+  //void set_timelog(void);
+  //const char *read_timelog(void);
 
-//  bool oled_task_user(void) {
-    //if (is_keyboard_master()) {
-      // If you want to change the display of OLED, you need to change here
-  //  oled_write_ln(read_layer_state(), false);
-  //  oled_write_ln(read_keylog(), false);
-  //  oled_write_ln(read_keylogs(), false);
-      //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
-      //oled_write_ln(read_host_led_state(), false);
-      //oled_write_ln(read_timelog(), false);
-    //} else {
-      //oled_write(read_logo(), false);
-    //}
-//    return false;
-  //}
-
-  //sprintf(wpm_str, "WPM: %03d", get_current_wpm());
-  //oled_write_P(PSTR("\n"), false);
-  //oled_write(wpm_str, false);
-
-  //static uint8_t zero_bar_count = 0;
-	//static uint8_t bar_count = 0;
-
-	//static void render_wpm_graph(void) {
-	//	uint8_t bar_height = 0;
-	//	uint8_t bar_segment = 0;
-
-	//	if (wpm_graph_timer == 0) {
-	//		wpm_graph_timer = timer_read();
-	//		return;
-	//		}
-	//	if(timer_elapsed(wpm_graph_timer) > 500) {
-	//		wpm_graph_timer = timer_read();
-
-	//		if(OLED_DISPLAY_HEIGHT == 64)
-	//			bar_height = get_current_wpm() / 2;
-	//		if(OLED_DISPLAY_HEIGHT == 32)
-	//			bar_height = get_current_wpm() / 4;
-	//		if(bar_height > OLED_DISPLAY_HEIGHT)
-	//			bar_height = OLED_DISPLAY_HEIGHT;
-
-	//		if(bar_height == 0) {
-	//				// keep track of how many zero bars we have drawn.  If
-	//				// there is a whole screen worth, turn the display off and 
-	//				// wait until there is something to do
-	//				if (zero_bar_count > OLED_DISPLAY_WIDTH) {
-	//					oled_off();
-	//					return;
-	//				}
-	//				zero_bar_count++;
-	//		} else
-	//			zero_bar_count=0;
-
-	//	oled_pan(false);
-	//	bar_count++;
-	//	for (uint8_t i = (OLED_DISPLAY_HEIGHT / 8); i > 0; i--) {
-	//		if (bar_height > 7) {
-	//			if (i % 2 == 1 && bar_count % 3 == 0)
-	//				bar_segment = 254;
-	//				else
-	//				bar_segment = 255;
-	//				bar_height -= 8;
-	//			} else {
-	//		switch (bar_height) {
-	//				case 0:
-	//			bar_segment = 0;
-	//			break;
-
-	//				case 1:
-	//			bar_segment = 128;
-	//			break;
-
-	//				case 2:
-	//			bar_segment = 192;
-	//			break;
-
-	//				case 3:
-	//			bar_segment = 224;
-	//			break;
-
-	//				case 4:
-	//			bar_segment = 240;
-	//			break;
-
-	//				case 5:
-	//			bar_segment = 248;
-	//			break;
-
-	//				case 6:
-	//			bar_segment = 252;
-	//			break;
-
-	//				case 7:
-	//			bar_segment = 254;
-	//			break;
-	//		}
-	//		bar_height = 0;
-
-	//		if (i % 2 == 1 && bar_count % 3 == 0)
-	//				bar_segment++;
-	//			}
-	//			oled_write_raw_byte(bar_segment, (i-1) * OLED_DISPLAY_WIDTH);
-	//}
-  //  }
-//}
-//
-//bool oled_task_user(void) {
-//    if (is_keyboard_master()) {
-//			render_wpm_graph();
-//    } else {
-//        render_wpm_graph();
-//    }
-//return false;
-//}
-//
+  bool oled_task_user(void) {
+    oled_write_ln(read_layer_state(), false);
+    oled_write_ln(read_keylog(), false);
+    oled_write_ln(read_keylogs(), false);
+    //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
+    //oled_write_ln(read_host_led_state(), false);
+    //oled_write_ln(read_timelog(), false);
+    return false;
+  }
 #endif // OLED_ENABLE
 
+
+//static bool shift_held = false;
+uint8_t mod_state;
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
     #ifdef OLED_ENABLE
