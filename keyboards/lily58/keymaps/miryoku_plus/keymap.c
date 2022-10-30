46ee817fe2c11957572e6ce109f2572969261b35
Josh O <josh.owen121@gmail.com>
2022-10-30 16:18:45 +0000

    qwerty layer

--- keyboards/lily58/keymaps/miryoku_plus/keymap.c
+++ keyboards/lily58/keymaps/miryoku_plus/keymap.c
@@ -64,8 +64,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_NO,       KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,             KC_PSCR, KC_F7,          KC_F8,         KC_F9,          KC_F12,         KC_NO,
       KC_NO,       KC_LCTL,        KC_LALT,        KC_LGUI,        KC_LSFT,        KC_NO,             KC_SCRL, KC_F4,          KC_F5,         KC_F6,          KC_F11,         KC_NO,
       KC_NO,       KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO, KC_NO,KC_NO,KC_PAUS, KC_F1,          KC_F2,         KC_F3,          KC_F10,         KC_NO,
-                                   KC_NO,          LT(6,KC_ESC),   KC_NO,          KC_NO,             KC_NO,   KC_NO,          KC_NO,         KC_NO
+                                   KC_NO,          LT(6,KC_ESC),   KC_NO,          KC_NO,             KC_NO,   KC_NO,          TG(7),         KC_NO
+      ),
+
+  // GAMING 
+	[7] = LAYOUT(
+      KC_NO,       KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,             KC_NO,  KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+      KC_NO,       KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,              KC_Y,   KC_U,           KC_I,           KC_O,           KC_P,        KC_NO,
+      KC_NO,       KC_A,           KC_S,           KC_D,           KC_F,           KC_G,              KC_H,   KC_J,           KC_K,           KC_L,           KC_SCLN,    KC_NO,
+      KC_NO,       KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,  KC_NO,KC_NO,KC_K,   KC_H,           KC_COMM,        KC_DOT,         KC_SLSH,        KC_NO,
+                                   KC_NO,          KC_ESC,         KC_SPC,         KC_TAB,            KC_ENT, KC_BSPC,        TG(7),           KC_NO
       )
+  
 };
 
 
